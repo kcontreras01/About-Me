4880212b0069351391f65510930731082dd85950
kcontreras01 <kiara@Kiaras-MacBook-Pro.local>
2017-07-15 13:54:23 -0400

    plugin
    attempting to add plugin

--- js/project.js
+++ js/project.js
@@ -1,36 +1,41 @@
-var myVariable = document.querySelector('clickme');
-alert('Hi! Welcome to my website.');
-
-var currentCount = 0;
-
-function updateCount() {
-	currentCount = currentCount + 1;
-	var newCountMessage;
-
-	if (currentCount == 1) {
-		newCountMessage = currentCount + " click! <br> Purr purr purr!";
-	} else	{
-		newCountMessage = currentCount + " clicks! <br> Meow meow meow!";
-	}
-
-document.getElementById("clickme").innerHTML = newCountMessage;
+$(document).ready(function() {
+  $('#fullpage').fullpage();
+});
 
 
-	if (currentCount == 10) {
-		document.body.style.backgroundColor = "#ffccff";
-	}
+// var myVariable = document.querySelector('clickme');
+// alert('Hi! Welcome to my website.');
 
-	else if (currentCount > 5 && currentCount < 10) {
-		document.body.style.backgroundColor = "  #cc00cc";
-	}
-	else if (currentCount == 2 || currentCount == 12) {
-		document.body.style.backgroundColor = " #99b3e6";
-	}
-	else if (currentCount > 20) {
-		document.body.style.backgroundColor = "#660066";
-	}
-	else {
-		document.body.style.backgroundColor = " #b3b3cc";
-	}
-}
-document.getElementById("clickme").onclick = updateCount;
+// var currentCount = 0;
+
+// function updateCount() {
+// 	currentCount = currentCount + 1;
+// 	var newCountMessage;
+
+// 	if (currentCount == 1) {
+// 		newCountMessage = currentCount + " click! <br> Purr purr purr!";
+// 	} else	{
+// 		newCountMessage = currentCount + " clicks! <br> Meow meow meow!";
+// 	}
+
+// document.getElementById("clickme").innerHTML = newCountMessage;
+
+
+// 	if (currentCount == 10) {
+// 		document.body.style.backgroundColor = "#ffccff";
+// 	}
+
+// 	else if (currentCount > 5 && currentCount < 10) {
+// 		document.body.style.backgroundColor = "  #cc00cc";
+// 	}
+// 	else if (currentCount == 2 || currentCount == 12) {
+// 		document.body.style.backgroundColor = " #99b3e6";
+// 	}
+// 	else if (currentCount > 20) {
+// 		document.body.style.backgroundColor = "#660066";
+// 	}
+// 	else {
+// 		document.body.style.backgroundColor = " #b3b3cc";
+// 	}
+// }
+// document.getElementById("clickme").onclick = updateCount;
